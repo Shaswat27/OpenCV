e546c1dc754161a59082e515cdae292bb6f2ab77
Shaswat27 <shaswatdatta@hotmail.com>
2015-12-05 18:06:23 +0530

    KLT updated

--- KLT_Feature_Tracker/KLT.cpp
+++ KLT_Feature_Tracker/KLT.cpp
@@ -7,3 +7,53 @@
 #include "opencv2/nonfree/nonfree.hpp"
 
 using namespace cv;
+
+Mat img_prev,img_next,gr_prev,gr_next;
+
+/// For FAST
+std::vector<cv::KeyPoint> FAST(Mat img);
+int thresh = 60;
+std::vector <cv::KeyPoint> keypoints_prev;
+std::vector <cv::KeyPoint> keypoints_next;
+
+
+
+int main(int argc, char** argv)
+{
+	/// Error check
+	if (argc!=3)
+	{
+		printf("Enter previous and next frame!\n");
+		return -1;
+	}
+
+	/// Load images
+	img_prev=imread(argv[1]);
+	img_next=imread(argv[2]);
+
+	/// Convert to grayscale
+	cvtColor( img_prev, gr_prev, CV_BGR2GRAY );
+	cvtColor( img_next, gr_next, CV_BGR2GRAY );
+
+	/// Find features in img_prev
+	keypoints_prev=FAST(gr_prev);
+
+	///  Apply KLT Tracker
+	vector<uchar> status;
+	vector<float> err;
+	std::vector<Point2f> prev,next;
+	prev = keypoints_prev.pt;
+	calcOpticalFlowPyrLK( gr_prev, gr_next, prev, next, status, err);
+	size_t i, k;
+    for( i = k = 0; i < next.size(); i++ )
+    {
+    	
+    }
+}
+
+std::vector<cv::KeyPoint> FAST(Mat img)
+{
+	std::vector<cv::KeyPoint> keypoints;
+	FASTX(img, keypoints, thresh, true, FastFeatureDetector::TYPE_9_16);
+	return keypoints;
+}
